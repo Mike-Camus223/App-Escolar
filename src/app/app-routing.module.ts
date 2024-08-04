20fbb27e3d6bd564b266b18efd908c7b2dc6331f
michael campos <michaelcamposvil@gmail.com>
2024-08-04 14:19:32 -0400

    rectificar sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'dashboard/admin', loadChildren: () => import('./features/dashboard/admin/admin.module').then(m => m.AdminModule) },
   { path: 'dashboard/user', loadChildren: () => import('./features/dashboard/user/user.module').then(m => m.UserModule) },
-  { path: 'dashboard', redirectTo: 'dashboard/admin', pathMatch: 'full' }, 
-  { path: '', loadChildren: () => import('./features/public/public.module').then(m => m.PublicModule) },
-  { path: '**', redirectTo: '' }
+  { path: 'dashboard', redirectTo: 'dashboard/admin', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard/admin', pathMatch: 'full' },  
+  { path: '**', redirectTo: 'dashboard/admin' }  
 ];
 
 @NgModule({
